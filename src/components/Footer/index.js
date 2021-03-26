312beef289060295c892a775d605fea7e0e20e57
michelvvs <michel.vvs@gmail.com>
2021-03-26 01:18:48 -0300

    corrigindo meu link do github no footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ const footer = () => {
     return (
         <section className="Footer">
             <p>
-                MikaFlix é uma criação de <a href="#"> Michel Victor.</a> <br/>
+                MikaFlix é uma criação de <a href="https://github.com/michelvvs" target="_blank" rel="noreferrer"> Michel Victor.</a> <br/>
                 Todos os dados foram usados da API do <a href="https://www.themoviedb.org/" target="_blank" rel="noreferrer">The Movie DB</a>.< br/>
                 Interface criada seguindo os tutoriais do canal do Bonieky Lacerda.
             </p>
